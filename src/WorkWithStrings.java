bab2eb11ab397aa505de2381509063215a067620
Vanessa <va.au@gmx.at>
2020-08-31 10:26:01 +0200

    revised 2

--- src/WorkWithStrings.java
+++ src/WorkWithStrings.java
@@ -3,16 +3,16 @@ import java.io.PrintStream;
 public class WorkWithStrings {
     public static void main(String[] args) {
 
-    String txt= " Hello World! ";
+        String txt = " Hello World! ";
 
-        System.out.printf("%s %s %d%n%n", "Die Länge des Strings: ", txt , txt.length());
+        System.out.printf("%s %s %d%n%n", "Die Länge des Strings: ", txt, txt.length());
         System.out.printf("%s%n%n", txt.toLowerCase());
         System.out.printf("%s%n%n", txt.toUpperCase());
         System.out.printf("%s%n%n", txt.replace("World", "Codersbay"));
         System.out.printf("%s%n%n%n", txt.stripLeading());
 
-        // ich komme nicht drauf wie ich da einen zeilenumbruch dazwischen einbauen kann oO
-        System.out.printf("%s%n", txt.repeat(15));
+        // ahhhh dankeschön :D
+        System.out.println((txt + "\n").repeat(15));
 
     }
 }
